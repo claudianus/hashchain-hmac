e9a0ea1ef9c3f079c2b00be2ecdf9577f4cc7345
claudianus <claudianus@engineer.com>
2021-04-11 01:36:53 +0900

    @

---
+++ hashchain-hmac/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace hashchain_hmac {
+  internal static class Program {
+    private static void Main() {
+      Console.WriteLine("hello world");
+      const int hashchainLength = 1000_0000;
+      var randomBytes = new byte[1000];
+      using var rng = new RNGCryptoServiceProvider();
+      rng.GetBytes(randomBytes);
+      var previousHash = randomBytes;
+      const string hmacKey = "hello world";
+      using var hmac = new HMACSHA256(Encoding.ASCII.GetBytes(hmacKey));
+      for (int i = 0; i < hashchainLength; i++) {
+        previousHash = hmac.ComputeHash(previousHash);
+        // var hashString = BitConverter.ToString(previousHash).Replace("-", string.Empty).ToLowerInvariant();
+        // Console.WriteLine($"{i} {hashString}");
+      }
+      Console.WriteLine("done");
+    }
+  }
+}
